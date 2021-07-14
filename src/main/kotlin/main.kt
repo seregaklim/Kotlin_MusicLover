569f65a52eac29705a03090c60b45484cbd37f9a
seregaklim <seregaklim@mail.ru>
2021-07-14 17:36:06 +0300

    first commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,24 +1,26 @@
-fun main () {
 
+fun main () {
     val product = 1500
     val quantity = 10
     val meloman = 0.01
-
-    var purchasePrice = product * quantity
+    val isRegularCustomer = true
+    val purchasePrice = product * quantity
     val discountSilver = product * quantity - 100
-    val discountGold = product * quantity * 0.05
-
-    var melomanPurchase = purchasePrice - (purchasePrice * meloman)
-    var melomanSilver = discountSilver- (discountSilver * meloman)
-    var milomanGold = discountGold - (discountGold * meloman)
-
-    if (purchasePrice >= 1000) {} else println("Без скидок $purchasePrice рублей")
-    if (purchasePrice >= 1001 && purchasePrice >= 10000) {}
-    else println("Со скидкой 100р, цена составляет $discountSilver рублей")
-    if (purchasePrice <= 10001) {} else println("Со скидкой 5 процентов, цена составляет $discountGold рублей")
-
-    if (purchasePrice >= 1000) {} else println("C дополнительной скидкой постоянного клиента $melomanPurchase рублей")
-    if (purchasePrice >= 1001 && purchasePrice >= 10000) {}
-    else println("C дополнительной скидкой постоянного клиента $melomanSilver рублей")
-    if (purchasePrice <= 10001) {} else println("C дополнительной скидкой постоянного клиента $milomanGold рублей")
+    val discountGold = purchasePrice - (product * quantity * 0.05).toInt()
+    val discont  = if (purchasePrice >= 1000) purchasePrice else if (purchasePrice >= 1001 && purchasePrice >= 10000)
+        discountSilver else if (purchasePrice <= 10001) discountGold else 0
+    val regularCustomer = purchasePrice - discont * meloman
+    val result = if (isRegularCustomer) regularCustomer else discont
+    println("$result рублей")
 }
+
+
+
+
+
+
+
+
+
+
+
